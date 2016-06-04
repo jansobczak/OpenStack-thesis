1907722bb86c292b1285d7cb22d821b1811c5846
Jan Sobczak <jasobeczek@gmail.com>
2016-06-04 17:47:24 +0200

    Openstackclient is broken ... changes for novaclient and others

--- OS_class.py
+++ OS_class.py
@@ -11,6 +11,12 @@ class OSTools(object):
     def toJSON(os_object):
         return json.dumps(os_object.to_dict(), sys.stdout, sort_keys=True, indent=4, separators=(',', ': '))
 
+    @staticmethod
+    def toSimpleTable(os_object):
+        print '{: <45} {: <20}'.format("ID", "Name")
+        for object in os_object:
+            print '{: <45} {: <20}'.format(object.id, object.name)
+
 
 class OSNova:
     'Class for openstack images'
@@ -29,13 +35,20 @@ class OSNova:
         for image in images:
             print OSTools.toJSON(image)
 
+    def getImages(self):
+        images = self.conn.image.images()
+        return images
+
+    def getFlavors(self):
+        return self.conn.compute.flavors()
+
     def showFlavors(self):
-        flavors = self.conn.compute.flavors()
+        flavors = self.getFlavors()
         for flavor in flavors:
             pprint.pprint(flavor)
 
     def showFlavorsJSON(self):
-        flavors = self.conn.compute.flavors()
+        flavors = self.getFlavors()
         for flavor in flavors:
             print OSTools.toJSON(flavor)
 
@@ -49,6 +62,9 @@ class OSNova:
         for server in servers:
             print OSTools.toJSON(server)
 
+    def getNetworks(self):
+        return self.conn.network.networks()
+
     def showNetworks(self):
         networks = self.conn.network.networks()
         for network in networks:
@@ -58,6 +74,47 @@ class OSNova:
         networks = self.conn.network.networks()
         for network in networks:
             print OSTools.toJSON(network)
+
+    def getKeyPairs(self):
+        return self.conn.compute.keypairs()
+
+    def getKeyPairs(self, keypair):
+        return self.conn.compute.find_keypair(keypair)
+
+    def showKeyPairs(self):
+        keypairs = self.conn.compute.keypairs()
+        for keypair in keypairs:
+            pprint.pprint(keypair)
+
+    def showKeyPairsJSON(self):
+        keypairs = self.conn.compute.keypairs()
+        for keypair in keypairs:
+            print OSTools.toJSON(keypair)
+
+    def createServer(self, image, flavor, network, keypair, server_name):
+        """Create server instance
+        Arguments:
+            image {[string]} -- Image ID or Name
+            flavor {[string]} -- Flavor ID or Name
+            network {[string]} -- Network ID or Name
+            keypair {[string]} -- Keypair ID or Name
+            server_name {[string]} -- Name for the new instance
+        """
+        print("Create server")
+        image = self.conn.compute.find_image(image)
+        flavor = self.conn.compute.find_flavor(flavor)
+        network = self.conn.network.find_network(network)
+        keypair = self.conn.compute.find_keypair(keypair, False)
+
+        server = self.conn.compute.create_server(
+            name=server_name,
+            image_id=image.id,
+            flavor_id=flavor.id,
+            networks=[{"uuid": network.id}],
+            key_name=keypair.name)
+
+        server = self.conn.compute.wait_for_server(server)
+        print server.created_at
 
 
 class OSConnection:
@@ -78,7 +135,7 @@ class OSConnection:
         self.prof.set_region(profile.Profile.ALL, '')
         self.conn = connection.Connection(
             profile=self.prof,
-            user_agent='reservation_service',
+            user_agent='sobczakj',
             auth_url=self.auth_url,
             project_name=self.project_name,
             username=self.username,
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from OS_class import OSConnection
 from OS_class import OSNova
+from OS_class import OSTools
 
 "Create connection class"
 "TODO config file to connection"
@@ -10,9 +11,26 @@ username = 'sobczakj'
 password = 'open.stack'
 
 os_conn = OSConnection(auth_url, project_name, username, password)
-os_image = OSNova(os_conn.getConn())
+os_nova = OSNova(os_conn.getConn())
 
-os_image.showImagesJSON()
-os_image.showFlavorsJSON()
-os_image.showServerJSON()
-os_image.showNetworkJSON()
+#os_nova.showImagesJSON()
+#os_nova.showFlavorsJSON()
+#os_nova.showServerJSON()
+#os_nova.showNetworkJSON()
+#os_nova.showKeyPairsJSON()
+
+#print "Images:"
+#OSTools.toSimpleTable(os_nova.getImages())
+
+#print "Flavors:"
+#OSTools.toSimpleTable(os_nova.getFlavors())
+
+#print "Networks:"
+#OSTools.toSimpleTable(os_nova.getNetworks())
+
+#print "KeyParis:"
+#OSTools.toSimpleTable(os_nova.getKeyPairs())
+
+OSTools.toJSON(os_nova.getKeyPairs("rest_service_key"))
+
+os_nova.createServer("Debian Jessie", "2", "rest_service_network", "rest_service_key", "API_instance")
