5f0a0bfb0f63ea318e2df41d38991ee5f078fea9
Jan Sobczak <>
2019-06-21 20:47:55 +0200

    Config update, fixes

--- tests/auth/auth.sh
+++ tests/auth/auth.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -s -H "Content-Type: application/json" -L -X POST --cookie-jar cookies -d @../examples/auth/admin.json http://localhost:8080/auth/ | python -m json.tool
+curl -s -H "Content-Type: application/json" -L -X POST --cookie-jar cookies -d @../../examples/auth/admin.json http://localhost:8080/auth/ | python -m json.tool
